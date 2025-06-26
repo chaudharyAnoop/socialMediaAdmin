49b7530041a0fe13965327b362f3a3b99914e4e7
chaudaryAnoop <anoop.kumar@appinventiv.com>
2025-06-26 16:51:53 +0530

    update code for formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import NavigationBar from "./components/NavigationBar/NavigationBar";
-import UsersList from "./pages/UserList/UserList";
-import AdminUserSearch from "./pages/adminUserSearch/AdminUserSearch";
-import AdminAllPosts from "./pages/AdminAllPost/AdminAllPost";
-import UserFollow from "./pages/UserFollow/userFollow";
-import AdminLogin from "./pages/AdminLogin/adminLogin";
-
+import { AppRouter } from "./pages/AppRouter";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.nav}>
       <div className={styles.main}>
-        <BrowserRouter>
-          <NavigationBar />
-          <Routes>
-            <Route path="/" element={<UsersList />} />
-            <Route path="/usersearch" element={<AdminUserSearch />} />
-            <Route path="/allposts" element={<AdminAllPosts />} />
-            <Route path="/follow" element={<UserFollow />} />
-            <Route path="/auth" element={<AdminLogin />} />
-          </Routes>
-        </BrowserRouter>
+        <AppRouter />
       </div>
     </div>
   );
